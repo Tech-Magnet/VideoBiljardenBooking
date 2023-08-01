5d263f748ef773ece788419197f7feb9323b0761
Luna Nordbergh <104737060+Luna0424@users.noreply.github.com>
2023-08-01 20:40:29 +0200

    Add Read and Write Support

--- public/main.js
+++ public/main.js
@@ -1,49 +1,68 @@
 /*WRITE TO DB*/
+
 // Get a reference to the database
 var database = firebase.database();
   
 // Add an event listener to the form submit
-document.getElementById("myForm").addEventListener("submit", function (event) {
+document.getElementById("bookingForm").addEventListener("submit", function (event) {
   event.preventDefault();
   
   // Get the data from the input field
-  var data = document.getElementById("dataInput").value;
+  var name = document.getElementById("txtName").value;
+  var phone = document.getElementById("txtPhone").value;
+  var day = document.getElementById("txtDay").value;
   
   // Write the data to the database
-  database.ref("messages").push().set({
-    text: data,
-    timestamp: firebase.database.ServerValue.TIMESTAMP,
+  database.ref("booking").push().set({
+    name: name,
+    phone: phone,
+    day: day
   });
   
   // Clear the input field after submitting
-  document.getElementById("dataInput").value = "";
+  document.getElementById("txtName").value = "";
+  document.getElementById("txtPhone").value = "";
 });
 
 
+
+
+
+
+
+
+
+
+
+
 /*READ FROM DATABASE*/
+
 // Get a reference to the database
 var database = firebase.database();
 // Get a reference to the table body
-var tableBody = document.querySelector("#messagesTable tbody");
+var tableBody = document.querySelector("#bookingTable tbody");
 
 // Attach a listener to the "messages" node to get real-time updates
-database.ref("messages").on("child_added", function (snapshot) {
-  var message = snapshot.val();
+database.ref("booking").on("child_added", function (snapshot) {
+  var book = snapshot.val();
   
   // Create a new table row
   var newRow = document.createElement("tr");
 
-  // Create two table cells for "text" and "timestamp"
-  var textCell = document.createElement("td");
-  var timestampCell = document.createElement("td");
+  // Create table cells
+  var nameCell = document.createElement("td");
+  var phoneCell = document.createElement("td");
+  var dayCell = document.createElement("td");
 
   // Set the content of the cells with the data from Firebase
-  textCell.textContent = message.text;
-  timestampCell.textContent = new Date(message.timestamp).toLocaleString();
+  nameCell.textContent = book.name;
+  phoneCell.textContent = book.phone;
+  dayCell.textContent = book.day;
 
   // Append the cells to the row
-  newRow.appendChild(textCell);
-  newRow.appendChild(timestampCell);
+  newRow.appendChild(nameCell);
+  newRow.appendChild(phoneCell);
+  newRow.appendChild(dayCell);
 
   // Append the row to the table body
   tableBody.appendChild(newRow);
