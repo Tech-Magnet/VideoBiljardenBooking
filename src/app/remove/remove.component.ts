cf711c04c15934d26a11975897b7df3fd4c22c61
Luna Nordbergh <104737060+Luna0424@users.noreply.github.com>
2023-09-22 02:09:53 +0200

    Update

--- src/app/remove/remove.component.ts
+++ src/app/remove/remove.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Query } from '@angular/core';
 
 import { initializeApp } from 'firebase/app';
 import { getAnalytics, logEvent } from "firebase/analytics";
-import { getDatabase, ref, get, query, orderByChild, equalTo, remove, DatabaseReference } from "firebase/database";
+import { getDatabase, ref, get, orderByChild, query, equalTo, } from 'firebase/database';
 
 @Component({
   selector: 'app-remove',
@@ -32,76 +32,81 @@ export class RemoveComponent {
     // Initialize Firebase
     const app = initializeApp(firebaseConfig);
     const analytics = getAnalytics(app);
-    const database = getDatabase(app);
-    //const database = getDatabase(app);
+    const database = getDatabase();
     logEvent(analytics, "Booking Removed");
 
     let day = this.formData.Day;
     let phone = this.formData.Phone;
     let table = this.formData.Table + this.formData.Week;
 
-
-    this.searchBookingsByPhone(database, phone, day, table)
-    .then((results) => {
+    /*this.searchBookingsByPhone(databaseRef, phone, day, table)
+    .then((results: any) => {
       // Handle the list of keys and data that match the phone number
-      results.forEach((result) => {
+      results.forEach((result: any) => {
         const key = result.key;
         const data = result.data;
         console.log('Key:', key);
         console.log('Data:', data);
-        
-        //Deleting Data
-        //const nodeRef: DatabaseReference = ref(database, "booking");
-        //const childRef: DatabaseReference = ref(nodeRef, key);
-//
-        //remove(childRef)
-        //  .then(function() {
-        //    console.log("Node deleted:", key);
-        //    console.log("Node Deleted from booking");
-        //  })
-        //  .catch(function(error: any) {
-        //    console.error("Error deleting node:", error);
-        //  });
       });
     })
-    .catch((error) => {
+    .catch((error: any) => {
       // Handle any errors that occur during the search
       console.error('Error:', error);
     });
+  }*/
+    //test(databaseRef, phone, day, table, key);
+    this.searchBookingsByPhone(database, phone, day, table)
+    .then((results: any) => {
+      console.log(results.key);
+    })
   }
 
 
-  // Define a function to search for bookings by phone and get their keys
-async searchBookingsByPhone(database:any, phone: string, day: string, table: string): Promise<{ key: string, data: any }[]> {
-  try {
-    // Create a reference to the "booking" location in the database
-    const bookingRef:DatabaseReference = ref(database, 'booking');
 
-    // Define a query to filter data by 'phone'
-    const queryByPhone = query(bookingRef, orderByChild('phone'), equalTo(phone));
 
-    // Get the data matching the query
-    const snapshot = await get(queryByPhone);
 
-    const keysAndData: { key: string, data: any }[] = [];
 
-    if (snapshot.exists()) {
-      snapshot.forEach((childSnapshot: any) => {
-        const key = childSnapshot.key; // Get the key of the child node
-        const data = childSnapshot.val(); // Get the data of the child node
-        if (data.day == day && data.table == table){
-          console.log("Data Exist, adding to remove list");
-          keysAndData.push({ key, data });
-        }else{
-          console.log("Data Not found");
-        }
-      });
+  async searchBookingsByPhone (database: any, phone: string, day: string, table: string): Promise<{ key: string, data: any }[]> {
+    try {
+      // Create a reference to the "booking" location in the database
+      const bookingRef: any = ref(database, 'booking');
+  
+      // Define a query to filter data by 'phone'
+      const queryByPhone = query(bookingRef, orderByChild('phone'), equalTo(phone));
+  
+      // Get the data matching the query
+      const snapshot = await get(queryByPhone);
+  
+      const keysAndData: { key: string, data: any }[] = [];
+  
+      if (snapshot.exists()) {
+        snapshot.forEach((childSnapshot: any) => {
+          const key = childSnapshot.key; // Get the key of the child node
+          const data = childSnapshot.val(); // Get the data of the child node
+          if (data.day == day && data.table == table && key != undefined){
+            //console.log("Data Exist, adding to remove list");
+            console.log("key " + key);
+            keysAndData.push({ key, data });
+          }else{
+            //console.log("Data Not found");
+          }
+        });
+      }
+  
+      return keysAndData;
+    } catch (error) {
+      console.error('Error searching for bookings by phone:', error);
+      throw error;
     }
-
-    return keysAndData;
-  } catch (error) {
-    console.error('Error searching for bookings by phone:', error);
-    throw error;
   }
-}
+  }
+
+
+
+function test(database: any,/* phoneToDelete: any, dayToDelete: any, tableToDelete: any, */key: any){
+  // Create a reference to the "booking" node
+  console.log("Starting Deletion with key: " + key);
+  const db_ref: any = ref(database, 'booking');
+
+  db_ref.child(key).remove();
 }
