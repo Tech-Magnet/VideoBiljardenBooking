7e9f8c9d6ef9bd2eb3072aa05e8d3fb22a1e4f5a
Luna Nordbergh <luna.nordbergh2@gmail.com>
2024-08-06 01:45:54 +0200

    Added support for table 3 cleanup function

--- cleardb/index.js
+++ cleardb/index.js
@@ -9,85 +9,82 @@ exports.cleardb = onRequest((request, response) => {
     const bookingRef = database.ref('booking');
 
 
-      (async () => {
+    (async () => {
         try {
-          await deleteOld("booking");
-          await deleteOld("admin");
-          logger.log("Old Data Deleted");
-        
-          await updateNew("booking");
-          await updateNew("admin");
-          logger.log("New Data Updated");
-        
-          // All database operations are completed here
-          // You can proceed with any other actions
-          logger.log("All Operations done, Exiting Program");
-          response.send("DONE!");
+            await deleteOld("booking");
+            await deleteOld("admin");
+            logger.log("Old Data Deleted");
+
+            await updateNew("booking");
+            await updateNew("admin");
+            logger.log("New Data Updated");
+
+            // All database operations are completed here
+            // You can proceed with any other actions
+            logger.log("All Operations done, Exiting Program");
+            response.send("DONE!");
         } catch (error) {
-          logger.error("Error:", error);
-          response.send("DONE!");
+            logger.error("Error:", error);
+            response.send("DONE!");
         }
-      })();
+    })();
 
 
-      // Call deleteOld for 'booking' and 'admin'
+    // Call deleteOld for 'booking' and 'admin'
     deleteOld('booking')
         .then(() => {
-          // Once deleteOld is done, call updateNew for 'booking'
-          return updateNew('booking');
-          response.send("DONE!");
+            // Once deleteOld is done, call updateNew for 'booking'
+            return updateNew('booking');
         })
         .then(() => {
-          // After updateNew for 'booking', call deleteOld for 'admin'
-          return deleteOld('admin');
+            // After updateNew for 'booking', call deleteOld for 'admin'
+            return deleteOld('admin');
         })
         .then(() => {
-          // Finally, call updateNew for 'admin'
-          return updateNew('admin');
+            // Finally, call updateNew for 'admin'
+            return updateNew('admin');
         })
         .then(() => {
-          // All operations are complete, you can now perform any final tasks
-          response.send("DONE!");
-          admin.app().delete();
+            // All operations are complete, you can now perform any final tasks
+            response.send("DONE!");
+            admin.app().delete();
         })
         .catch((error) => {
-          logger.error('Error:', error);
+            logger.error('Error:', error);
         });
-
-
 
 
     async function deleteOld(node) {
         const NodeRef = database.ref(node);
-        const valuesToDelete = ["Bord_1", "Bord_2", "Bord_3"];
-      
+        const valuesToDelete = ["Bord_1", "Bord_2", "Bord_3", "Bord_4"];
+
         const promises = valuesToDelete.map(valueToDelete => {
-          const query = NodeRef.orderByChild("table").equalTo(valueToDelete);
-        
-          return query.once("value")
-            .then(snapshot => {
-              const deletionPromises = [];
-              snapshot.forEach(childSnapshot => {
-                const keyToDelete = childSnapshot.key;
-                deletionPromises.push(NodeRef.child(keyToDelete).remove());
-                deletionPromises.push(logger.info("DELETED CHILD: " + keyToDelete));
-              });
-              return Promise.all(deletionPromises);
-            });
+            const query = NodeRef.orderByChild("table").equalTo(valueToDelete);
+
+            return query.once("value")
+                .then(snapshot => {
+                    const deletionPromises = [];
+                    snapshot.forEach(childSnapshot => {
+                        const keyToDelete = childSnapshot.key;
+                        deletionPromises.push(NodeRef.child(keyToDelete).remove());
+                        deletionPromises.push(logger.info("DELETED CHILD: " + keyToDelete));
+                    });
+                    return Promise.all(deletionPromises);
+                });
         });
-      
+
         await Promise.all(promises);
-      
+
         return Promise.resolve();
     }
 
     async function updateNew(node) {
         var NodeRef = database.ref(node);
-        var valuesToSearch = ["Bord_1_c", "Bord_2_c", "Bord_3_c"];
-    
+        var valuesToSearch = ["Bord_1_c", "Bord_2_c", "Bord_3_c", "Bord_4_c"];
+
         // Create an array to store promises for queries
         var promises = [];
-    
+
         // Loop through the values and create a query for each one
         valuesToSearch.forEach(function (value) {
             var query = NodeRef.orderByChild("table").equalTo(value);
@@ -104,24 +101,25 @@ exports.cleardb = onRequest((request, response) => {
 
         // Wait for all queries to complete
         Promise.all(promises)
-        .then(function (results) {
-            const TableArr_m = [];
-        
-            results.forEach(function (snapshotData) {
-                for (const key in snapshotData) {
-                    if (snapshotData.hasOwnProperty(key)) {
-                        var currentTable = snapshotData[key].table;
-                        var updatedTable = currentTable.replace("_c", "");
-                        TableArr_m.push({ key: key, updatedTable: updatedTable });
-                    }
-                }
-            });
+            .then(function (results) {
+                const TableArr_m = [];
 
-            // Now, TableArr_m contains objects with keys and updatedTable values
-              
+                results.forEach(function (snapshotData) {
+                    for (const key in snapshotData) {
+                        if (snapshotData.hasOwnProperty(key)) {
+                            var currentTable = snapshotData[key].table;
+                            var updatedTable = currentTable.replace("_c", "");
+                            TableArr_m.push({key: key, updatedTable: updatedTable});
+                        }
+                    }
+                });
+
+                // Now, TableArr_m contains objects with keys and updatedTable values
+
+
                 // Update the nodes in the database
                 TableArr_m.forEach(function (entry) {
-                    NodeRef.child(entry.key).update({ "table": entry.updatedTable })
+                    NodeRef.child(entry.key).update({"table": entry.updatedTable})
                         .then(function () {
                             logger.log("Node updated:", node);
                             logger.log("Node Updated in " + node);
@@ -134,7 +132,9 @@ exports.cleardb = onRequest((request, response) => {
             })
             .catch(function (error) {
                 logger.error("Error querying data:", error);
+
             });
     }
+
     return null;
-  });
+});
