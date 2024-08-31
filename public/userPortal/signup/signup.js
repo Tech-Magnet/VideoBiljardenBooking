5b4a5a9cca3ac9688574c96b2bf085da4776ed25
Luna Nordbergh <luna.nordbergh2@gmail.com>
2024-08-31 13:02:40 +0200

    Update signup.js

--- public/userPortal/signup/signup.js
+++ public/userPortal/signup/signup.js
@@ -5,14 +5,7 @@ import { initializeAppCheck, ReCaptchaV3Provider } from "https://www.gstatic.com
 import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAxv9AQ5b9Ig9HnCAzxfLcHfdojZiGMyNQ",
-  authDomain: "videobiljardenorebrosite.firebaseapp.com",
-  databaseURL: "https://videobiljardenorebrosite-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "videobiljardenorebrosite",
-  storageBucket: "videobiljardenorebrosite.appspot.com",
-  messagingSenderId: "1042889427467",
-  appId: "1:1042889427467:web:bbbedfe2ce5eea96d8d50e",
-  measurementId: "G-FPJBGKX7R0"
+  /*API KEYS*/
 };
 
 const app = initializeApp(firebaseConfig);
